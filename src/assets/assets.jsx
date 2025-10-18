7e46e5ae43cf641cf2f2bc586478c0676ed10b70
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-10-18 20:18:12 +0200

    add an list of object to manage all the image and title of the card

---
+++ src/assets/assets.jsx
@@ -0,0 +1,19 @@
+import suzume from '../assets/images/suzume.jpg';
+import friren from '../assets/images/friren.jpg';
+import weathering from '../assets/images/weathering.jpg';
+import your_name from '../assets/images/your_name.jpg';
+
+
+
+
+export const movies = [
+    {title: 'suzume', Image: suzume},
+    {title: 'friren', Image: friren},
+    {title: 'weathering', Image: weathering},
+    {title: 'your_name', Image: your_name}
+]
+
+
+
+
+
--- src/components/CardGrid/CardGrid.jsx
+++ src/components/CardGrid/CardGrid.jsx
@@ -2,20 +2,25 @@ import Card from '../card/Card';
 
 import './cardgrid.css';
 
-import poster1 from '../../assets/images/suzume.jpg';
-import poster2 from '../../assets/images/friren.jpg';
-import poster3 from '../../assets/images/weathering.jpg';
-import poster4 from '../../assets/images/your_name.jpg';
+// import poster1 from '../../assets/images/suzume.jpg';
+// import poster2 from '../../assets/images/friren.jpg';
+// import poster3 from '../../assets/images/weathering.jpg';
+// import poster4 from '../../assets/images/your_name.jpg';
+
+import { movies } from '../../assets/assets';
 
 
 function CardGrid(){
 
     return (
         <div className='card-grid'>
-        <Card movie={poster1} title={'suzume'} />
+        {movies.map((movie, index) =>(
+            <Card key={index} title={movie.title} movie={movie.Image} />
+        ) )}
+        {/* <Card movie={poster1} title={'suzume'} />
         <Card movie={poster2} title={'friren'}/>
         <Card movie={poster3} title={'weathering with you'}/>
-        <Card movie={poster4} title={'your name'} />
+        <Card movie={poster4} title={'your name'} /> */}
         </div>
     )
 };
