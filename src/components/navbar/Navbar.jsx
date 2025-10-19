c3c41a572f436284d62642a260ddd500f0f7383f
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-10-19 13:47:45 +0200

    overlay added to the navbar, next we need to improve the style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,25 @@
 
 import './Navbar.css' ;
 
+import { useState } from 'react';
+import Overlay from "../../components/overlay/Overlay";
+
 
 
 function Navbar(){
+
+    const[isSearchOpen, setIsSearchOpen] = useState(false);
 
     return (
         <nav className="navbar" >
             <h1 className="logo" >Movie Library</h1>
             <form className="search-form" >
-                <input placeholder="Search movie ..." />
-                <button>Search</button>
+                <input onClick={()=> setIsSearchOpen(true)} placeholder="Search movie ..." />
             </form>
+            <Overlay
+            isOpen={isSearchOpen}
+            onClose={()=>setIsSearchOpen(false)}
+            />
             <ul className="nav-link" >
                 <li><a>All movie</a></li>
                 <li><a>About</a></li>
---
+++ src/components/overlay/Overlay.jsx
@@ -0,0 +1,82 @@
+import './overlay.css';
+import { useState } from 'react';
+
+import React from 'react'
+
+function Overlay({isOpen, onClose}) {
+
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+
+    if(!isOpen)return null;
+
+    const handleSearch = async (e) => {
+    const value = e.target.value;
+    setQuery(value);
+
+    if (value.trim() === "") {
+        setResults([]);
+        return;
+    }
+
+    try {
+        const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=5f394899920a1e49322ba143100fd64f&query=${encodeURIComponent(value)}`,
+        {
+            headers: {
+            accept: "application/json",
+            },
+        }
+        );
+
+        if (!res.ok) throw new Error("Network error");
+        const data = await res.json();
+
+        console.log("Search results:", data); 
+        setResults(data.results?.slice(0, 10) || []);
+    } catch (error) {
+        console.error("Error searching movie:", error);
+    }
+    };
+
+    return (
+        <div className='overlay' >
+            <div className='overlay-content' >
+                <button className='close-btn' onClick={onClose} >X</button>
+                <input 
+                    type='text'
+                    placeholder='Search for a movie'
+                    value={query}
+                    onChange={handleSearch}
+                    autoFocus
+                />
+                <div className='results' >
+                    {results.length > 0 ? (
+                        results.map((movie) => (
+                            <div key={results.id} className='result-card' >
+                                <img 
+                                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                                <p>{movie.title}</p>
+                            </div>
+                        ))
+                    ) : (
+                        query && <p>No results found</p>
+                    )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Overlay
+
+
+
+
+
+
+
+
