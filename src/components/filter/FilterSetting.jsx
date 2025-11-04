cfee7399f04d413cf04335416a2a73f1b4ebf694
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-11-04 06:54:16 +0100

    improve all movie page styling

--- src/components/filter/FilterSetting.jsx
+++ src/components/filter/FilterSetting.jsx
@@ -8,7 +8,42 @@ function FilterSetting(){
 
     return(
         <div className="filter-setting" >
-            <h2>Filter Setting Component</h2>
+            <h2>Filter Setting</h2>
+            <div className="filter-selector" >
+                <div className="genre" >
+                    <label>Filter by Genre:</label>
+                    <select>
+                        <option>All</option>
+                        <option>Action</option>
+                        <option>Comedy</option>
+                        <option>Drama</option>
+                    </select>
+                </div>
+                <div className="rating" >
+                    <label>Filter by Rating:</label>
+                    <select>
+                        <option>All</option>
+                        <option>G</option>
+                        <option>PG</option>
+                        <option>PG-13</option>
+                        <option>R</option>
+                    </select>
+                </div>
+                <div className="year" >
+                    <label>Filter by Year:</label>
+                    <select>
+                        <option>All</option>
+                        <option>2020s</option>
+                        <option>2010s</option>
+                        <option>2000s</option>
+                        <option>1990s</option>
+                    </select>
+                </div>
+                <div className="apply-button" >
+                    <button>Apply Filters</button>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,13 +1,18 @@
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./searchbar.css";
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import { faSearchDollar, faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar(){
+
+    
 
     return(
 
         <div className="search-bar" >
             <input placeholder="Search movie ..." />
-            <button>search</button>
+            <button> <FontAwesomeIcon icon={faSearchPlus} /> </button>
         </div>
 
     )
